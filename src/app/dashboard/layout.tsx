a23340ca6b098583c9945b5c343a0026f65007a1
Maurizio <mauriziomatic1990@gmail.com>
2026-09-12 12:17:44 +0200

    feat: localize layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -51,7 +51,7 @@ export default async function DashboardLayout({ children }: { children: ReactNod
         </Link>
       )}
 
-      {!hideFeedbackTools && <FeedbackWidget />}
+      {!hideFeedbackTools && <FeedbackWidget locale={permissions.organizationLocale} />}
     </div>
   );
 }
