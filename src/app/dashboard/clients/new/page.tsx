614323a5fbd3cee074f87c481ca698bf8d562351
Maurizio <mauriziomatic1990@gmail.com>
2026-09-12 19:18:00 +0200

    refactor: align new client form values

--- src/app/dashboard/clients/new/page.tsx
+++ src/app/dashboard/clients/new/page.tsx
@@ -23,7 +23,6 @@ export default async function NewClientPage() {
             phone: "",
             email: "",
             note: "",
-            internal_note: "",
           }}
         />
       </div>
